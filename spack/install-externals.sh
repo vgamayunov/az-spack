a77fa459ebcda9af73ce8a7eb1e7c9fc95e354b3
Victor Gamayunov <vgamayunov@microsoft.com>
2024-04-04 15:40:32 +0000

    more spack install scripts

--- spack/install-externals.sh
+++ spack/install-externals.sh
@@ -2,6 +2,14 @@
 
 . setenv.sh
 
+# Do not assign arch or compiler to the externals
+spack install hpcx-mpi
+spack install hcoll
+spack install ucx
+spack install ucc
+
+exit 0
+
 spack install hpcx-mpi %$SPACK_BASE_COMPILER target=$SPACK_BASE_TARGET
 spack install hcoll %$SPACK_BASE_COMPILER target=$SPACK_BASE_TARGET
 spack install ucx %$SPACK_BASE_COMPILER target=$SPACK_BASE_TARGET
---
+++ spack/install-libs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+. setenv.sh
+
+# GCC+HPCX
+spack install --reuse netcdf-fortran %$SPACK_BASE_COMPILER target=$SPACK_BASE_TARGET ^hpcx-mpi
+
+# Intel compilers + Open MPI
+spack install --reuse netcdf-fortran target=$SPACK_BASE_TARGET %intel@2021.10.0 ^openmpi%intel fabrics=hcoll,knem,ucc,ucx schedulers=slurm \
+  ^cmake%$SPACK_BASE_COMPILER ^gmake%$SPACK_BASE_COMPILER ^hdf5%intel ^netcdf-c%intel
+
+# Intel compilers + Intel MPI
+spack install --reuse netcdf-fortran target=$SPACK_BASE_TARGET %intel@2021.10.0 ^intel-oneapi-mpi \
+  ^gmake%$SPACK_BASE_COMPILER ^hdf5%intel ^cmake%$SPACK_BASE_COMPILER ^netcdf-c%intel ^bzip2%$SPACK_BASE_COMPILER \
+  ^c-blosc%$SPACK_BASE_COMPILER ^cmake%$SPACK_BASE_COMPILER
