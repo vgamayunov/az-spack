85f38a06b33ca0341732884652c3577a9a8ab7b7
Victor Gamayunov <vgamayunov@microsoft.com>
2024-06-24 17:20:57 +0100

    fix symlinks in publisher and server

--- cvmfs/setup-server.sh
+++ cvmfs/setup-server.sh
@@ -9,15 +9,14 @@ fi
 mkdir -pv /data0/{cvmfs,cvmfs-srv}
 mkdir -pv /srv
 
-[ -d /var/spool/cvmfs ] && mv /var/spool/cvmfs /var/spool/cvmfs-save
-[ -d /srv/cvmfs ] && mv /srv/cvmfs /srv/cvmfs-save
-ln -sf /data0/cvmfs /var/spool/cvmfs
-ln -sf /data0/cvmfs-srv /srv/cvmfs
-
-
 wget https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest_all.deb
 sudo dpkg -i cvmfs-release-latest_all.deb
 rm -f cvmfs-release-latest_all.deb
 sudo apt-get update
 sudo apt-get install -y cvmfs cvmfs-server
 # sudo DEBIAN_FRONTEND=noninteractive apt-get install -y cvmfs cvmfs-server
+
+[ -d /var/spool/cvmfs ] && mv /var/spool/cvmfs /var/spool/cvmfs-save
+[ -d /srv/cvmfs ] && mv /srv/cvmfs /srv/cvmfs-save
+ln -sf /data0/cvmfs /var/spool/cvmfs
+ln -sf /data0/cvmfs-srv /srv/cvmfs
