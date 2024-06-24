8297e820ec9659bbd89fa36767b760df8471f00e
Victor Gamayunov <vgamayunov@microsoft.com>
2024-06-24 17:15:00 +0100

    fix sybminks in publisher install

--- cvmfs/install-publisher.sh
+++ cvmfs/install-publisher.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 set -euo pipefail
 
+# Publisher may discard cvmfs directories when rebooted
+mkdir -pv /mnt/resource/cvmfs_srv /mnt/resource/cvmfs
+[ -d /var/spool/cvmfs ] && mv /var/spool/cvmfs /var/spool/cvmfs-save
+[ -d /srv/cvmfs ] && mv /srv/cvmfs /srv/cvmfs-save
+ln -sf /mnt/resource/cvmfs /var/spool/cvmfs
+ln -sf /mnt/resource/cvmfs_srv /srv/cvmfs
+
 wget https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest_all.deb
 dpkg -i cvmfs-release-latest_all.deb
 rm -f cvmfs-release-latest_all.deb
 apt-get update
 apt-get install -y cvmfs cvmfs-server
-
-# Publisher may discard cvmfs directories when rebooted
-mkdir -pv /mnt/resource/cvmfs_srv /mnt/resource/cvmfs
-ln -sf /mnt/resource/cvmfs /var/spool/cvmfs
-ln -sf /mnt/resource/cvmfs_srv /srv/cvmfs
 
 systemctl stop autofs
 systemctl disable autofs
