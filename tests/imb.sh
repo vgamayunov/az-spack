4d1d1f1e75859881965630d722f4c341c1372926
Victor Gamayunov <vgamayunov@microsoft.com>
2024-06-25 17:58:22 +0100

    add imb.sh as the first test of the environment

---
+++ tests/imb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -p hbv4
+#SBATCH -N 2
+#SBATCH --ntasks-per-node=1
+
+. /cvmfs/ape.core/setenv.sh
+spack load intel-mpi-benchmarks%intel^openmpi
+
+np=$((SLURM_JOB_NUM_NODES * SLURM_TASKS_PER_NODE))
+mpirun IMB-MPI1 Allreduce -npmin $np
